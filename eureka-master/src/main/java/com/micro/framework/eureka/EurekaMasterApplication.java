e6a67e4d5d631aeed7304ca89fedc29dca483b65
zhoujian <zhoujian@newtank.cn>
2018-12-27 14:40:14 +0800

    micro-framework

---
+++ eureka-master/src/main/java/com/micro/framework/eureka/EurekaMasterApplication.java
@@ -0,0 +1,11 @@
+package com.micro.framework.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tb
+ * @date 2018/12/27 14:38
+ */
+@SpringBootApplication
+public class EurekaMasterApplication {
+}
