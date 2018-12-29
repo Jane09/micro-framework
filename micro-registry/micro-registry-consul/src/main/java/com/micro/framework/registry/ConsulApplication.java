f10981aa7603f0feefba00a5c6518db14c4bda3d
zhoujian <zhoujian@newtank.cn>
2018-12-29 15:07:33 +0800

    micro-framework

--- micro-registry/micro-registry-consul/src/main/java/com/micro/framework/registry/ConsulApplication.java
+++ micro-registry/micro-registry-consul/src/main/java/com/micro/framework/registry/ConsulApplication.java
@@ -1,8 +1,19 @@
 package com.micro.framework.registry;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 /**
  * @author tb
  * @date 2018/12/29 15:00
  */
+@SpringBootApplication
 public class ConsulApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConsulApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
 }
