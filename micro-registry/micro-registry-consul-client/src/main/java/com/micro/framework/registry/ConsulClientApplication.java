12947523c944002f182ac4dd51a7f82f63e78389
zhoujian <zhoujian@newtank.cn>
2019-01-02 16:22:15 +0800

    micro-framework

--- micro-registry/micro-registry-consul-client/src/main/java/com/micro/framework/registry/ConsulClientApplication.java
+++ micro-registry/micro-registry-consul-client/src/main/java/com/micro/framework/registry/ConsulClientApplication.java
@@ -1,11 +1,16 @@
 package com.micro.framework.registry;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author tb
@@ -20,8 +25,36 @@ public class ConsulClientApplication {
         new SpringApplicationBuilder(ConsulClientApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello world";
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 获取所有服务
+     */
+    @RequestMapping("/services")
+    public Object services() {
+        return discoveryClient.getInstances("registry-consul");
+    }
+
+    /**
+     * 从所有服务中选择一个服务（轮询）
+     */
+    @RequestMapping("/discover")
+    public Object discover() {
+        return loadBalancerClient.choose("registry-consul").getUri().toString();
+    }
+
+    @RequestMapping("/call")
+    public String call() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("registry-consul");
+        System.out.println("服务地址：" + serviceInstance.getUri());
+        System.out.println("服务名称：" + serviceInstance.getServiceId());
+
+        String callServiceResult = new RestTemplate().getForObject(serviceInstance.getUri().toString() + "/hello", String.class);
+        System.out.println(callServiceResult);
+        return callServiceResult;
     }
 }
--- micro-registry/micro-registry-consul/src/main/java/com/micro/framework/registry/ConsulApplication.java
+++ micro-registry/micro-registry-consul/src/main/java/com/micro/framework/registry/ConsulApplication.java
@@ -3,6 +3,9 @@ package com.micro.framework.registry;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
@@ -14,11 +17,18 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @date 2018/12/29 15:00
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class ConsulApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConsulApplication.class)
                 .web(WebApplicationType.SERVLET)
                 .run(args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "helle consul";
     }
 }
