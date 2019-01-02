b698ec75c683ea438b8d0af8f7f2e98ba3cbb439
zhoujian <zhoujian@newtank.cn>
2019-01-02 11:56:42 +0800

    micro-framework

---
+++ micro-generator/src/main/java/com/micro/framework/generator/GeneratorApplication.java
@@ -0,0 +1,17 @@
+package com.micro.framework.generator;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author tb
+ * @date 2019/1/2 11:49
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(GeneratorApplication.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
