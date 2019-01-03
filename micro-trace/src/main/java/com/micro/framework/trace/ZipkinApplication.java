d3ce359a76cb95d0682623679cd537516385ea81
zhoujian <zhoujian@newtank.cn>
2019-01-03 15:27:46 +0800

    micro-framework

--- micro-trace/src/main/java/com/micro/framework/trace/ZipkinApplication.java
+++ micro-trace/src/main/java/com/micro/framework/trace/ZipkinApplication.java
@@ -2,14 +2,12 @@ package com.micro.framework.trace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin2.server.internal.EnableZipkinServer;
 
 /**
  * @author tb
  * @date 2019/1/2 16:41
  */
 @SpringBootApplication
-@EnableZipkinServer
 public class ZipkinApplication {
 
     public static void main(String[] args) {
