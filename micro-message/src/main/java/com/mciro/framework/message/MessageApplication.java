4ac1b82687b8e5795da4ee7a0428554abc244bdb
zhoujian <zhoujian@newtank.cn>
2019-01-03 11:45:02 +0800

    micro-framework

---
+++ micro-message/src/main/java/com/mciro/framework/message/MessageApplication.java
@@ -0,0 +1,8 @@
+package com.mciro.framework.message;
+
+/**
+ * @author tb
+ * @date 2019/1/3 11:41
+ */
+public class MessageApplication {
+}
---
+++ micro-message/src/main/java/com/mciro/framework/message/model/MessageDTO.java
@@ -0,0 +1,18 @@
+package com.mciro.framework.message.model;
+
+import lombok.Getter;
+import lombok.Setter;
+/**
+ * 100% 消息传输成功
+ * @author tb
+ * @date 2019/1/3 11:42
+ */
+@Getter
+@Setter
+public class MessageDTO {
+
+    private Long msgId;
+    //关联上一个msgId 判断是否漏传
+    private Long pmsgId;
+    private String message;
+}
